f09ca798ed96a383bc3520cd7295a673333f67a3
sreenath <rdgowda79@gmail.com>
2025-10-08 14:17:21 +0530

    Frst commit with data bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms'
+// <app-root></app-root>
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
-  title = 'bookapp';
+  imageUrl = "favicon.ico";
+  title: string = 'bookapp';
+  url: string = 'http://www.google.com';
+  name: string = "Ford";
+  isSubscribed=true;
+  gender = "Male";
+
+  logInfo() {
+    console.log("Logging info !");
+  }
+  changeImage() {
+    if (this.imageUrl == "favicon.ico")
+      this.imageUrl = "ford.jpg";
+    else
+      this.imageUrl = "favicon.ico";
+
+  }
 }
+// source - code behind
