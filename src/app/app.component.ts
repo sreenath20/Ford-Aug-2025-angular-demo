de974e7ebd07539c18fd457e695b60eb991b320e
sreenath <rdgowda79@gmail.com>
2025-10-09 11:55:39 +0530

    Added structural directives demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms'
+import { CommonModule, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { Product } from './model/Products';
 // <app-root></app-root>
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, NgIf, NgFor, NgSwitch,NgSwitchCase,NgSwitchDefault],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
 })
 export class AppComponent {
+  userType:string="";
+
+  fruits: string[] | null = ['Apple', 'Orange', 'Kiwi'];// null;
+
+  products: Product[] = [];
+  constructor() {
+    this.products.push({
+      id: 1,
+      name: 'Laptop',
+      price: 100
+    });
+    this.products.push({ id: 22, name: 'Laptop 2', price: 200 });
+    this.products.push({ id: 3, name: 'Laptop 3', price: 300 });
+    this.products.push({ id: 44, name: 'Laptop 4', price: 400 });
+  }
+
+
   imageUrl = "favicon.ico";
   title: string = 'bookapp';
   url: string = 'http://www.google.com';
   name: string = "Ford";
-  isSubscribed=true;
+  isSubscribed = true;
   gender = "Male";
 
   logInfo() {
@@ -29,3 +48,4 @@ export class AppComponent {
   }
 }
 // source - code behind
+
